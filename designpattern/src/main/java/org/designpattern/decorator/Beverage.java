980a55f828bea86ad573e8fb6f5e31a938332f89
JMsuper <tjsqls2067@naver.com>
2023-12-28 12:21:10 +0900

    add decorator pattern IO example

--- designpattern/src/main/java/org/designpattern/decorator/Beverage.java
+++ designpattern/src/main/java/org/designpattern/decorator/Beverage.java
@@ -1,12 +1,13 @@
 package org.designpattern.decorator;
 
+import java.io.InputStream;
+
 public abstract class Beverage {
     public String description;
 
     public Beverage(String description){
         this.description = description;
     }
-
     public String getDescription() {
         return description;
     }
